1d5309c256ca6393231cd9be0d5f9f77cf0ea196
ydzzz0096 <ydzzz.0096@gmail.com>
2025-09-16 23:32:33 +0800

    yeah

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -2,4 +2,4 @@
 mkdir -p build
 cd build
 cmake .. && make -j
-./pc_demo
+./pc_test
--- scripts/stress.sh
+++ scripts/stress.sh
@@ -4,4 +4,4 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release .. && make -j
 # 直接运行
-./pc_demo
+./pc_test
